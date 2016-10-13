51e205a81eca0a21cf925a505a0730bfc938ec0c
SimulatedGREG <simulatedgreg@gmail.com>
2016-10-13 02:27:06 -0500

    build

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container">
+    <heading></heading>
+    <resume></resume>
+  </div>
+</template>
+
+<script>
+import Heading from 'components/Heading'
+import Resume from 'components/Resume'
+
+export default {
+  components: {
+    Heading,
+    Resume
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    margin: 60px auto;
+    max-width: 650px !important;
+    padding: 20px;
+  }
+</style>

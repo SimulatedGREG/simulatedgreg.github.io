ccba52dae9587b7b8f5f55cd8b4a305d8b24d83b
SimulatedGREG <simulatedgreg@gmail.com>
2016-10-18 00:19:01 -0500

    add ga

--- src/main.js
+++ src/main.js
@@ -11,5 +11,11 @@ Vue.use(Resource)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    ga('send', {
+      hitType: 'pageview',
+      page: '/'
+    })
+  }
 })
